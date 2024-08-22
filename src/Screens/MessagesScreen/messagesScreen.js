d2037f7be89a3f25e8dd55e0ee831ee3658b2629
ah-lasheen <alasheen005@gmail.com>
2024-08-22 10:47:43 -0400

    messages page

---
+++ src/Screens/MessagesScreen/messagesScreen.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Picker, Button, Image } from 'react-native';
+import {useNavigation} from '../../navigation';
+import HeaderButtons from '../../components/HeaderButtons/HeaderButtons';
+
+const MessagesScreen = () => {
+
+  const navigation = useNavigation();
+  
+  const profileOne = () => {
+
+  }
+
+  const profilTwo = () => {
+    
+  }
+
+  const profileThree = () => {
+    
+  }
+
+  const profileFour = () => {
+    
+  }
+
+  return (
+    <>
+      <TouchableOpacity styles="">
+  
+      </TouchableOpacity>
+    </>
+  )
+
+}
+
+
+
+
+export default MessagesScreen;
--- src/navigation/index.js
+++ src/navigation/index.js
@@ -9,6 +9,7 @@ import ConfirmEmailScreen from '../Screens/ConfirmEmailScreen/ConfirmEmailScreen
 import ForgotPasswordScreen from '../Screens/ForgotPasswordScreen/ForgotPasswordScreen';
 import ResetPasswordScreen from '../Screens/ResetPasswordScreen/ResetPasswordScreen';
 import FindPartner from '../Screens/HomeScreen(FindPartner)/FindPartner'
+import MessagesScreen from '../Screens/MessagesScreen/messagesScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -16,14 +17,13 @@ const Navigation = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                
+                <Stack.Screen name="Messages" component={MessagesScreen} />
                 <Stack.Screen name="Login" component={SignInScreen} />
                 <Stack.Screen name="Register" component={SignUpScreen} />
                 <Stack.Screen name="Confirm" component={ConfirmEmailScreen} />
                 <Stack.Screen name="Forgot Password" component={ForgotPasswordScreen} />
                 <Stack.Screen name="New Password" component={ResetPasswordScreen} />
                 <Stack.Screen name="Home" component={FindPartner} />
-                
             </Stack.Navigator>
         </NavigationContainer>
     )
